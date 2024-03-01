2ba0a3acdc0820264224fdfb9cf109a3014ac675
GeorgePh0 <george.philip0020@gmail.com>
2024-03-01 18:09:38 +0000

    Add Navbar style and Routes to the links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,36 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">
+                <NavLink to="/">
+                    <Navbar.Brand>
                     Rate My Car
-                </Navbar.Brand>
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                        <Nav.Link><i className="fas fa-sign-in"></i>Sign In</Nav.Link>
-                        <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
+                        <NavLink
+                            exact
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/"
+                        >
+                            <i className="fas fa-home"></i>Home
+                        </NavLink>
+                        <NavLink
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/signin">
+                                <i className="fas fa-sign-in"></i>Sign In
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup"><i className="fas fa-user-plus"></i>Sign Up</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
