d6663686c009f908855eac47b3eb1fadd841fa43
GeorgePh0 <george.philip0020@gmail.com>
2024-03-08 11:53:40 +0000

    Add handleSignOut function to sign out users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
 
@@ -10,6 +10,16 @@ import axios from "axios";
 const NavBar = () => {
 
     const currentUser = useCurrentUser();
+    const SetCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post('dj-rest-auth/logout/');
+            SetCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const addPostIcon = (
         <NavLink
@@ -36,7 +46,7 @@ const NavBar = () => {
           >
             <i className="fas fa-heart"></i>Liked
           </NavLink>
-          <NavLink className={styles.NavLink} to="/" onClick={() => {}}>
+          <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
             <i className="fas fa-sign-out-alt"></i>Sign out
           </NavLink>
           <NavLink
